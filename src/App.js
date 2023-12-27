3cfdca8b13f712f142ed01bd2bad3f2d1acc1072
THossen <tan.hossen.1@gmail.com>
2023-12-26 20:09:33 -0500

    default route set

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} /> 
         <Route path="/Home" element={<Home />} />
         <Route path="/ShowCase" element={<ShowCase />} />
         <Route path="/Contact" element={<ContactUs />} />
@@ -21,3 +22,4 @@ function App() {
 }
 
 export default App;
+
