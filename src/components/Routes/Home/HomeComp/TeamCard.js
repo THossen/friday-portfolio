fe31d71addea48bbab2d96108332a1b3bfc234ba
THossen <tan.hossen.1@gmail.com>
2023-12-26 19:57:05 -0500

    minor changes

--- src/components/Routes/Home/HomeComp/TeamCard.js
+++ src/components/Routes/Home/HomeComp/TeamCard.js
@@ -9,7 +9,7 @@ const TeamCard = () => {
           key={id}
           className="max-w-md rounded-lg overflow-hidden shadow-lg m-4"
         >
-          <img className="w-full" src={imageUrl}/>
+          <img className="w-full" src={imageUrl} alt=""/>
           <div className="px-4 py-2">
             <div className="font-bold text-xl">{name}</div>
             <p className="text-gray-700 text-base font-semibold mb-2 border-b-2 border-b-tertiary">{role}</p>
